0fef31bf42ee8ea39323f674be03f41ce5a8219d
Jerome Manceau <jmanc3@gmail.com>
2023-01-02 13:24:26 +1100

    change server url to fetch in client

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async ( e ) => {
 
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:3600', {
+    const response = await fetch('https://gpt3-server.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
