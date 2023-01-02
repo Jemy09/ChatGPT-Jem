592a1f3c710ae8ecd52ce5cf23395acff50c9444
Jerome Manceau <jmanc3@gmail.com>
2023-01-02 13:47:35 +1100

    removed slash in fetched URL

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async ( e ) => {
 
     loader(messageDiv);
 
-    const response = await fetch('https://gpt3-server.onrender.com/', {
+    const response = await fetch('https://gpt3-server.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
